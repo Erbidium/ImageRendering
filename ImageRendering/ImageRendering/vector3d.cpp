56a8de796205f7f70569a23fcc74a013f594ea8f
Oleksandr <OlexandrTanasienko@gmail.com>
2021-05-15 23:06:28 +0300

    Overload operators +, -, * for vector3d. Implement cross product for vector3d
    Move implementations of methods of class vector3d to .cpp

--- ImageRendering/ImageRendering/vector3d.cpp
+++ ImageRendering/ImageRendering/vector3d.cpp
@@ -1,1 +1,59 @@
 ﻿#include "vector3d.h"
+
+vector3d::vector3d():
+x(0), y(0), z(0)
+{}
+
+vector3d::vector3d(double newX, double newY, double newZ):
+x(newX), y(newY), z(newZ)
+{}
+
+double vector3d::getX() const
+{
+	return x;
+}
+
+double vector3d::getY() const
+{
+	return y;
+}
+
+double vector3d::getZ() const
+{
+	return z;
+}
+
+void vector3d::setX(double newX)
+{
+	x = newX;
+}
+
+void vector3d::setY(double newY)
+{
+	y = newY;
+}
+
+void vector3d::setZ(double newZ)
+{
+	z = newZ;
+}
+
+vector3d vector3d::operator+(vector3d a) const
+{
+	return {x + a.getX(), y + a.getY(), z + a.getZ()};
+}
+
+vector3d vector3d::operator-(vector3d a) const
+{
+	return {x - a.getX(), y - a.getY(), z - a.getZ()};
+}
+
+double vector3d::operator*(vector3d a) const
+{
+	return x * a.getX() + y * a.getY() + z * a.getZ();
+}
+
+vector3d vector3d::crossProduct(vector3d a) const
+{
+	return {y * a.getZ() - z * a.getY(), z * a.getX() - x * a.getZ(), x * a.getY() - y * a.getX()};
+}
--- ImageRendering/ImageRendering/vector3d.h
+++ ImageRendering/ImageRendering/vector3d.h
@@ -6,12 +6,16 @@ class vector3d
 	double y;
 	double z;
 public:
-	vector3d():x(0),y(0),z(0){}
-	vector3d(double newX, double newY, double newZ):x(newX),y(newY),z(newZ){}
-	double getX()const{return x;}
-	double getY()const{return y;}
-	double getZ()const{return z;}
-	void setX(double newX){x=newX;}
-	void setY(double newY){y=newY;}
-	void setZ(double newZ){z=newZ;}
+	vector3d();
+	vector3d(double newX, double newY, double newZ);
+	double getX() const;
+	double getY() const;
+	double getZ() const;
+	void setX(double newX);
+	void setY(double newY);
+	void setZ(double newZ);
+	vector3d operator+(vector3d a) const;
+	vector3d operator-(vector3d a) const;
+	double operator*(vector3d a) const;
+	vector3d crossProduct(vector3d a) const;
 };
