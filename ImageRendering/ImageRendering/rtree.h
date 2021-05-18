5298e05293676c7c75a9909624d0a96a63621e4b
Oleksandr <OlexandrTanasienko@gmail.com>
2021-05-18 17:21:03 +0300

    Partially implement methods of class rtree

--- ImageRendering/ImageRendering/rtree.h
+++ ImageRendering/ImageRendering/rtree.h
@@ -9,16 +9,22 @@ class rtree
     const int maxNumberOfLeafs=5;
     std::vector<rtree*> childs;
     std::vector<triangle> triangles;
-    rtree(){}
-    rtree(triangle newTriangle)
+    rtree * root;
+    rtree():root(nullptr){}
+    rtree(triangle newTriangle):root(nullptr)
     {
         triangles.push_back(newTriangle);
     }
-    rtree(std::vector<rtree*> childs)
-    {
-        triangles.push_back(newTriangle);
-    }
+    std::vector<rtree*> getChildren(){return childs;}
 public:
+    void doInsert(std::vector<rtree*>&splitNodes)
+    {
+
+    }
+    void adjustBounds()
+    {
+
+    }
     void insert(triangle newTriangle)
     {
         std::vector<rtree*>splitNodes;
@@ -27,17 +33,34 @@ public:
         adjustBounds();
         if(splitNodes.size()>0)
         {
-            rtree * newRoot;
+            childs.clear();
+            for(int i=0;i<splitNodes.size();i++)
+            {
+                childs.push_back(splitNodes[i]);
+            }
         }
     }
     void chooseLeaf(std::vector<rtree*>&splitNodes, rtree * currentTreeNode)
     {
-        
+        if(currentTreeNode->getChildren().size()==0)
+        {
+            doInsert(splitNodes);
+            adjustBounds();
+        }
+        else
+        {
+            ///choose minimal growth of volume
+            rtree * optimalNode;/////
+            chooseLeaf(splitNodes, optimalNode);
+            adjustBounds();
+            if(splitNodes.size()>0)
+            {
+                if(childs.size()<maxEntries)
+                    ////unwritten code
+            }
     }
-    void adjustBounds()
-    {
-
-    }
+    
+    
 }
 };
 
